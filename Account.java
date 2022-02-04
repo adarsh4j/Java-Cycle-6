0b230b54f909a42ada83f82b7d130728bbbbf360
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-02-04 14:51:04 +0530

    By throw Exception

---
+++ Account.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Account{ 
+
+	static void Balance(int money){ 		if(money<500) 
+
+			throw new ArithmeticException("Sorry,balance amount  is less than 500"); 
+
+		else
+
+			System.out.println("Money is withdrawed"); 
+
+	} 
+
+	public static void main(String args[]){
+
+		System.out.println("How many money you need");
+
+		Scanner obj = new Scanner(System.in);	
+
+		int a = obj.nextInt();	
+
+		Balance(a);
+
+		
+
+	} 
+
+}
